6eeae9b83dccd612b85e12e0570d1e4b407929eb
Douglas Berkley <douglasmberkley@gmail.com>
2021-11-19 11:44:28 +0900

    fixed validation misspelling

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,8 +1,8 @@
 class Experience < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
-  valdates :name, presence: true
-  valdates :price, presence: true
+  validates :name, presence: true
+  validates :price, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   # TODO: The seeds don't have proper addresses
