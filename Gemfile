ff1f0c00762afbc8381d076838d9228214fdb0be
Douglas Berkley <douglasmberkley@gmail.com>
2021-11-19 11:37:57 +0900

    added bookings and geocoding

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'cloudinary', '~> 1.16.0'
 gem 'devise'
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 gem 'font-awesome-sass'
+gem 'geocoder'
 gem 'simple_form'
 
 group :development, :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -59,28 +59,61 @@ if User.count < 30
   puts "...Created #{User.count} users"
 end
 
+puts "Cleaning up experiences and bookings..."
+Booking.destroy_all
+Experience.destroy_all
+puts "Detroyed experiences..."
 
 puts "Creating experiences..."
 experiences = {
-  'Russian Roulette' => ""
-  'Lying on Train Tracks' => ""
-  'Cliff Jumping' => ""
-  'Blindfold Driving' => ""
-  'Jetskiing with Sharks' => ""
-  'Swimming with Alligators' => ""
-  'Hippo Rodeo' => ""
-  'Mountain Biking Everest' => ""
-  'Helicopter Obstacle Course' => ""
-  'Highway Car Jumping' => ""
+  'Russian Roulette' => { image_url: 'https://d4r15a7jvr7vs.cloudfront.net/ewoJICAgICAgICAgICAgICAgICJidWNrZXQiOiAiZmlsZXMubGJyLmNsb3VkIiwKCSAgICAgICAgICAgICAgICAia2V5IjogInB1YmxpYy93a29pMWx4aTlkbzRwNm5yb2J4OC5qcGciLAoJICAgICAgICAgICAgICAgICJlZGl0cyI6IHsKCSAgICAgICAgICAgICAgICAgICJyZXNpemUiOiB7CgkgICAgICAgICAgICAgICAgICAgICJ3aWR0aCI6IDk0NSwKCSAgICAgICAgICAgICAgICAgICAgImhlaWdodCI6IDUyNiwKCSAgICAgICAgICAgICAgICAgICAgImZpdCI6ICJjb3ZlciIKCSAgICAgICAgICAgICAgICAgIH0KCSAgICAgICAgICAgICAgICB9CgkgICAgICAgICAgICB9' }
+  'Lying on Train Tracks' => { image_url: 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX2198605.jpg' }
+  'Cliff Jumping' => { image_url: 'https://img1.10bestmedia.com/Images/Photos/317046/photo_54_990x660.jpg' }
+  'Blindfold Driving' => { image_url: 'https://www.nottinghamoffroadevents.co.uk/_webedit/cached-images/331-0-0-0-10000-10000-536.jpg' }
+  'Jetskiing with Sharks' => { image_url: 'https://img.redbull.com/images/c_crop,x_0,y_0,h_2848,w_3164/c_fill,w_860,h_782/q_auto,f_auto/redbullcom/2018/07/06/c8eb4913-6df0-4bd7-b6e9-4f24f02c0e81/jetskiing-collection' }
+  'Swimming with Alligators' => { image_url: 'https://www.hola.com/us/images/025e-0f96b9f13dd6-18f93d88afaf-1000/square-800/gator.jpg' }
+  'Hippo Rodeo' => { image_url: 'https://cdn.hswstatic.com/gif/hippo-sunscreen.jpg' }
+  'Mountain Biking Everest' => { image_url: 'https://i.dailymail.co.uk/i/pix/2012/06/17/article-2160665-13A64880000005DC-860_634x416.jpg' }
+  'Helicopter Obstacle Course' => { image_url: 'https://corporatehelicopters.b-cdn.net/wp-content/uploads/america-sailing-cup-720x480.jpg' }
+  'Highway Car Jumping' => { image_url: 'https://media.gq.com/photos/55828968e52bc4b477a972b1/1:1/w_450,h_450,c_limit/blogs-the-feed-man-jumps-from-car-635.jpg' }
 }
 prices = [500, 1000, 1500, 2000, 2500, 3000]
 
 experiences.each do |name, info|
-  Experience.create!(
+  experience = Experience.create!(
     name: name,
     description: Faker::Lorem.paragraph,
-    price: prices.sample
-    address: Faker::Address.street_address
+    price: prices.sample,
+    address: Faker::Address.street_address,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
     user: User.all.sample
   )
+  file = URI.open(info[:image_url])
+  experience.photo.attach(io: file, filename: 'experience.png', content_type: 'image/png')
 end
+puts "Created #{Experience.count} experiences..."
+
+puts "Creating 2 bookings for each user"
+User.find_each do |user|
+  experiences = Experience.all.sample(2)
+  experiences.each do |experience|
+
+  end
+  booking = Booking.new(
+    user: user,
+    experience: Experience.all.sample,
+    start_time: DateTime.now + rand(5..15).days
+  )
+  booking.end_time = booking.start_time + rand(1..3).hours
+  bookings.save!
+
+  booking = Booking.new(
+    user: user,
+    experience: Experience.all.sample,
+    start_time: DateTime.now + rand(5..15).days
+  )
+  booking.end_time = booking.start_time + rand(1..3).hours
+  bookings.save!
+end
+puts "Created #{Bookings.count} bookings..."
