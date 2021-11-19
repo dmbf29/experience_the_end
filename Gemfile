03f30bc827335244dca3ec9bb206fe710dfc105b
Douglas Berkley <douglasmberkley@gmail.com>
2021-11-19 10:28:29 +0900

    added user seeds

--- Gemfile
+++ Gemfile
@@ -33,9 +33,9 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'autoprefixer-rails', '10.2.5'
 gem 'cloudinary', '~> 1.16.0'
 gem 'devise'
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 gem 'font-awesome-sass'
 gem 'simple_form'
-
 
 group :development, :test do
   gem 'pry-byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
+require 'open-uri'
+
 puts 'Getting Admin users...'
-doug = User.find_by(email: 'douglasmberkley@gmail.com') || User.create(email: 'douglasmberkley@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true)
-trouni = User.find_by(email: 'trouni@gmail.com') || User.create(email: 'trouni@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true)
-sasha = User.find_by(email: 'sasha.kaverina@gmail.com') || User.create(email: 'sasha.kaverina@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true)
-yann = User.find_by(email: 'yann.luc.klein@gmail.com') || User.create(email: 'yann.luc.klein@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true)
-derek = User.find_by(email: 'derek.n.torres@gmail.com') || User.create(email: 'derek.n.torres@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true)
-noemi = User.find_by(email: 'noemi.calanducci@gmail.com') || User.create(email: 'trouni@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true)
+doug = User.find_by(email: 'douglasmberkley@gmail.com') || User.create(email: 'douglasmberkley@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true, first_name: 'Doug', last_name: 'Berkley')
+trouni = User.find_by(email: 'trouni@gmail.com') || User.create(email: 'trouni@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true, first_name: 'Trouni', last_name: 'Tiet')
+sasha = User.find_by(email: 'sasha.kaverina@gmail.com') || User.create(email: 'sasha.kaverina@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true, first_name: 'Sasha', last_name: 'Kaverina')
+yann = User.find_by(email: 'yann.luc.klein@gmail.com') || User.create(email: 'yann.luc.klein@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true, first_name: 'Yann', last_name: 'Klein')
+derek = User.find_by(email: 'derek.n.torres@gmail.com') || User.create(email: 'derek.n.torres@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true, first_name: 'Derek', last_name: 'Torres')
+noemi = User.find_by(email: 'noemi.calanducci@gmail.com') || User.create(email: 'noemi.calanducci@gmail.com', password: ENV['ADMIN_PASSWORD'], admin: true, first_name: 'Noemi', last_name: 'Ashizuka')
+
+unless doug.photo.attached?
+  file = file = URI.open('https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1523933095/viqfqp0tfkmcwmj7cfwe.jpg')
+  doug.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+end
+
+unless trouni.photo.attached?
+  file = file = URI.open('https://avatars.githubusercontent.com/u/34345789?v=4')
+  trouni.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+end
+
+unless sasha.photo.attached?
+  file = file = URI.open('https://avatars.githubusercontent.com/u/63218144?v=4')
+  sasha.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+end
+
+unless yann.photo.attached?
+  file = file = URI.open('https://avatars.githubusercontent.com/u/26819547?v=4')
+  yann.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+end
+
+unless derek.photo.attached?
+  file = file = URI.open('https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1625501928/uti26nayfjxxsytkim0k.jpg')
+  derek.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+end
+
+unless noemi.photo.attached?
+  file = file = URI.open('https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1581465654/ald6cwc2teqzuzavawdt.jpg')
+  noemi.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+end
+
+members = [doug, trouni, sasha, yann, derek, noemi]
+
+puts "Cleaning all other users..."
+Users.where.not(id: members).destroy_all
+
+30.times do
+  file = URI.open('https://thispersondoesnotexist.com/image')
+  user = Upload.create!(
+    email: Faker::Internet.safe_email,
+    password: '123123',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+  user.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
+end
+
+puts "...Created #{User.count} users"
