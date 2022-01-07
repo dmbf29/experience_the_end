9c393f4c53dd27338642f7804193184cb7887638
Douglas Berkley <douglasmberkley@gmail.com>
2022-01-07 15:37:58 +0900

    added new valiations for experience and booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :experience
-  validates :start_time, presence: true
+  validates :date, presence: true
+  validates :participants, length: { minimum: 1 }, allow_blank: false
   enum status: { pending: 0, accepted: 1, rejected: 2 }
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,10 +1,10 @@
 class Experience < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
-  HOURS = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5]
   validates :name, presence: true
   validates :price, presence: true
-  validates :hours, inclusion: { in: HOURS }
+  validates :start_time, presence: true
+  validates :end_time, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   # TODO: The seeds don't have proper addresses
