a0f7b378a151eab36b8e556303422dcee6fd379b
Douglas Berkley <douglasmberkley@gmail.com>
2022-01-07 15:10:52 +0900

    removed end_time validation from model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :experience
   validates :start_time, presence: true
-  validates :end_time, presence: true
   enum status: { pending: 0, accepted: 1, rejected: 2 }
 end
