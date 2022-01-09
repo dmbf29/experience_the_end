d61c1810436370d3f881c774e29c953edc3f68b1
Douglas Berkley <douglasmberkley@gmail.com>
2022-01-09 14:25:45 +0900

    Update to enum
    Co-authored-by: Trouni <34345789+trouni@users.noreply.github.com>

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -12,7 +12,7 @@ class Experience < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   # TODO: The seeds don't have proper addresses
-  scope :active, -> { where(status: 'active') }
+  enum status: { building: 'building', active: 'active' }
 
   def start_time_formatted
     start_time.strftime('%l%p')
