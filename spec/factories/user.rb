3a374647e7af3d6dac7528781b12c5223c2691a0
Douglas Berkley <douglasmberkley@gmail.com>
2022-06-10 14:27:58 +0900

    cleaned up factory bot syntax

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
-  factory :account do
-    email "testuser@example.com"
-    first_name "Billy"
-    last_name "Scammer"
-    password "123123"
-    admin false
+  factory :user do
+    email { "testuser@example.com" }
+    first_name { "Billy" }
+    last_name { "Scammer" }
+    password { "123123" }
+    admin { false }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  user = build(:user)
 
   describe '#initialize' do
     context 'when valid' do
       it 'generates a valid user with all columns' do
+        user = build(:user)
         expect(user.valid?).to eq(true)
       end
     end
 
     context 'when invalid' do
-      context 'without email' do
-        before do
-          user.email = nil
-          user.password = nil
-        end
-
+      context 'without email and password' do
         it 'generates an invalid user' do
+          user = build(:user, email: nil, password: nil)
           expect(user.valid?).to eq(false)
         end
 
         it 'generates an error message' do
+          user = build(:user, email: nil, password: nil)
           user.valid?
           expect(user.errors.messages).to eq({ email: ["can't be blank"], password: ["can't be blank"] })
         end
