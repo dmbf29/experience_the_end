cacea2c5047cfe13756b8e893ec480c21e13fa21
Douglas Berkley <douglasmberkley@gmail.com>
2022-06-10 14:23:17 +0900

    added use intialize unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user = build(:user)
+
+  describe '#initialize' do
+    context 'when valid' do
+      it 'generates a valid user with all columns' do
+        expect(user.valid?).to eq(true)
+      end
+    end
+
+    context 'when invalid' do
+      context 'without email' do
+        before do
+          user.email = nil
+          user.password = nil
+        end
+
+        it 'generates an invalid user' do
+          expect(user.valid?).to eq(false)
+        end
+
+        it 'generates an error message' do
+          user.valid?
+          expect(user.errors.messages).to eq({ email: ["can't be blank"], password: ["can't be blank"] })
+        end
+      end
+    end
+  end
+
+  # describe '#truncated_details' do
+  #   it 'returns truncated details' do
+  #     expect(user.truncated_details).to eq('Once upon a...')
+  #   end
+  # end
 end
